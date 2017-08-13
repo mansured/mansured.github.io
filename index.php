7dffe1ca56c8954c5484c49906c73e2f7faa9a4c
mansured <mansure@list.ru>
2017-08-13 23:29:14 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>push-notifications</title>
+        <meta name="description" content="push-notifications">
+        <meta name="author" content="push-notifications">
+        <link rel="stylesheet" href="css/styles.css">
+        <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <script type="text/javascript" src="//www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
+        <script type="text/javascript" src="/firebase_subscribe.js"></script>
+        <script type="text/javascript" src="scripts.js"></script>
+        <button type="button" id="subscribe">Следить за изменениями</button>
+    </body>
+</html>
